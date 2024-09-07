20ea273ad4a753dd407c90af4ed9659b9ecbf815
Azsa Nurwahyudi <azsa.nwhy@gmail.com>
2024-09-07 19:02:23 +0700

    feat(home): add link to GitHub profile in main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)]'>
       <main>
-        <h1 className='text-2xl md:text-[32px] font-bold text-light-heading dark:text-dark-heading mb-2'>WORK IN PROGRESS...</h1>
-        <p className="text-sm text-light-text dark:text-dark-text">&copy; {new Date().getFullYear()} AZSA NURWAHYUDI.</p>
+        <h1 className='text-2xl md:text-[32px] font-bold text-light-heading dark:text-dark-heading mb-2'>
+          WORK IN PROGRESS...
+        </h1>
+        <p className='text-sm text-light-text dark:text-dark-text'>
+          &copy; {new Date().getFullYear()}{' '}
+          <Link
+            href='https://github.com/azsanrd'
+            target='_blank'
+            rel='noreferrer noopener'
+            className='hover:underline'
+          >
+            AZSA NURWAHYUDI.
+          </Link>
+        </p>
       </main>
     </div>
   );
